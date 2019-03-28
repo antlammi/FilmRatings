2e015985812478ea14bac688eea1b6a6a5506957
antlammi <antti.lammi@helsinki.fi>
2019-03-28 19:33:07 +0200

    initial implementation of logging in and adding users

--- application/films/views.py
+++ application/films/views.py
@@ -1,12 +1,15 @@
 from application import app, db
+from flask_login import login_required
 from flask import redirect, render_template, request, url_for
 from application.films.models import Film
 from application.films.forms import FilmForm
 @app.route("/films/new")
+@login_required
 def films_form():
     return render_template("films/new.html", form = FilmForm())
 
 @app.route("/films/", methods=["POST"])
+@login_required
 def films_create():
     form = FilmForm(request.form)
 
@@ -19,7 +22,9 @@ def films_create():
     db.session().commit()
 
     return redirect(url_for("films_index"))
-
+##update
+#@app.route("/films/<film_id>", methods=["POST"])
+#@login_required
 @app.route("/films/", methods=["GET"])
 def films_index():
     return render_template("films/list.html", films = Film.query.all())
---
+++ application/user/views.py
@@ -0,0 +1,23 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+
+from application import app, db
+from application.auth.models import User
+from application.user.forms import UserForm
+@app.route("/user/new")
+def signup_form():
+    return render_template("user/new.html", form = UserForm())
+
+@app.route("/user/", methods=["POST"])
+def signup():
+    form = UserForm(request.form)
+    
+    u = User(form.name.data, form.username.data, form.password.data)
+    ##f.director = f.director.data
+
+    if not form.validate():
+        return render_template("user/new.html", form = form)
+    db.session().add(u)
+    db.session().commit()
+
+    return redirect(url_for("films_index"))
