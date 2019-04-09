fde00ec656fcd549a632f23bd914c8ded26bd572
antlammi <antti.lammi@helsinki.fi>
2019-04-09 20:31:23 +0300

    Director field in film forms changed to SelectField

--- application/films/forms.py
+++ application/films/forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, SelectField, validators
+
+#Tämä dynaaminen SelectField ei varsinaisesti tarvitse validointia, mutta hajotti StringField kentän validoinnin. Tämä manuaalisesti ohittaa tämän ongelman.
+class MySelectField(SelectField):
+    def pre_validate(self, form):
+        pass
+
 
 class FilmForm(FlaskForm):
-    name = StringField("Film name", [validators.Length(min=1)])
-    director = StringField("Directed by")
+    name = StringField("Film name", [validators.Length(min=1, max=400)])
+    director = MySelectField(u'Director', choices=[], coerce=int)
+      
     class Meta:
         csrf = False
-
-
--- application/films/views.py
+++ application/films/views.py
@@ -7,21 +7,23 @@ from application.films.forms import FilmForm
 @app.route("/films/new")
 @login_required("ANY")
 def films_form():
-    return render_template("films/new.html", form = FilmForm())
+    formtorender = FilmForm() 
+    formtorender.director.choices = [(d.id, d.name) for d in Director.query.all()]
+    return render_template("films/new.html", form=formtorender)
 
 @app.route("/films/", methods=["POST"])
 @login_required(role="ANY")
 def films_create():
     form = FilmForm(request.form)
-
-    f = Film(form.name.data)
-
-    director = Director.query.filter_by(name=form.director.data).first()
     
-    f.director_id = director.id
-    ##check if null, create director with name if so
-       
+    f = Film(form.name.data)
+    d_id = form.director.data
+   
+    f.director_id = d_id
+    
+    
     if not form.validate():
+        form.director.choices = [(d.id, d.name) for d in Director.query.all()]
         return render_template("films/new.html", form = form)
     db.session().add(f)
     db.session().commit()
@@ -31,7 +33,9 @@ def films_create():
 @app.route("/films/<film_id>/edit", methods=["GET"])
 @login_required(role="ADMIN")
 def films_edit(film_id):
-    return render_template("films/update.html", form=FilmForm(), film_id = film_id)
+    formtorender = FilmForm() 
+    formtorender.director.choices = [(d.id, d.name) for d in Director.query.all()]
+    return render_template("films/update.html", form=formtorender, film_id = film_id)
 
 @app.route("/films/<film_id>", methods=["GET"])
 def films_show(film_id):
@@ -54,11 +58,12 @@ def films_update(film_id):
     f = Film.query.get(film_id)
 
     f.name = form.name.data
+    d_id = form.director.data
+   
+    f.director_id = d_id
 
-    director = Director.query.filter_by(name=form.director.data).first()
-
-    f.director_id = director.id
     if not form.validate():
+        form.director.choices = [(d.id, d.name) for d in Director.query.all()]
         return render_template("films/new.html", form = form)
     db.session().commit()
     return redirect(url_for("films_index"))
