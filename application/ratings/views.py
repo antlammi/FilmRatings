c912ccbdf5aede99a972fc39e3935ab465ef1e61
antlammi <antti.lammi@helsinki.fi>
2019-04-21 21:14:54 +0300

    rating score sort reversed

--- application/ratings/views.py
+++ application/ratings/views.py
@@ -27,7 +27,7 @@ def reviews_sorted(sortby):
         reviews = sorted(reviews, key=lambda rating:rating[0])
     
     if (sortby =='score'):
-        reviews = sorted(reviews, key=lambda rating:rating[4])
+        reviews = sorted(reviews, key=lambda rating:rating[4], reverse = True)
     if (sortby == 'user'):
         reviews= sorted(reviews, key =lambda rating:rating[2])
     
