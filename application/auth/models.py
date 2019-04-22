1c732b2838642b8d828e0147572f40b9da8317b0
antlammi <antti.lammi@helsinki.fi>
2019-04-22 11:02:21 +0300

    added a basic user profile page

--- application/auth/models.py
+++ application/auth/models.py
@@ -2,6 +2,8 @@ from application import db
 from application.models import Base
 from sqlalchemy.orm import relationship
 from application.ratings.models import Rating
+
+from sqlalchemy.sql import text
 class User(Base):
     __tablename__ = "account"
 
@@ -31,3 +33,51 @@ class User(Base):
 
     def role(self):
         return self.urole
+
+    @staticmethod 
+    def favorite_films(id):
+        stmt = text("SELECT Film.name, Film.id, Rating.score AS avg FROM Rating, Film, Account"
+        " WHERE Film.id = Rating.film_id AND Rating.user_id = Account.id AND Account.id = :id ORDER BY avg DESC LIMIT 5").params(id = id)
+        res = db.engine.execute(stmt)
+        favorite = []
+        for row in res:
+            favorite.append([row[0], row[1], row[2]])
+
+        return favorite
+
+    @staticmethod
+    def favorite_actors(id):
+        stmt = text("SELECT Actor.name, Actor.id, AVG(Rating.score) AS avg FROM Actor, Rating, Film_actor, Film, Account"
+        " WHERE Film_actor.actor_id = Actor.id AND film_actor.film_id = Film.id "
+        " AND Film.id = Rating.film_id AND Rating.user_id = Account.id AND Account.id = :id "
+        " GROUP BY Actor.id ORDER BY avg DESC LIMIT 5").params(id = id)
+        res = db.engine.execute(stmt)
+        favorite = []
+        for row in res:
+            favorite.append([row.name, row.id, round(row.avg, 2)])
+
+        return favorite
+
+    @staticmethod
+    def favorite_directors(id):
+        stmt = text("SELECT Director.name, Director.id, AVG(Rating.score) AS avg FROM Director, Rating, Film, Account "
+        "WHERE Director.id = Film.director_id AND Film.id = Rating.film_id AND Rating.user_id = Account.id AND Account.id = 2 "
+        "GROUP BY Director.name, Director.id ORDER BY avg DESC LIMIT 5").params(id = id)
+        res = db.engine.execute(stmt)
+        favorite = []
+        for row in res:
+            favorite.append([row[0], row[1], row[2]])
+
+        return favorite
+
+    @staticmethod
+    def user_reviews(id):
+        stmt = text("SELECT Film.name, Film.id, Account.username, Account.id, rating.score, rating.review FROM Rating, Account, Film" 
+        " WHERE length(Rating.review) > 0 AND Rating.user_id = Account.id AND Rating.film_id = Film.id AND Account.id = :id ORDER BY Rating.date_modified DESC").params(id=id)
+        res = db.engine.execute(stmt)
+        reviews = []
+        for row in res:
+            reviews.append([row[0], row[1], row[2], row[3], row[4], row[5]])
+
+        return reviews
+
--- application/user/views.py
+++ application/user/views.py
@@ -4,6 +4,7 @@ from flask_login import login_user, logout_user
 from application import app, db
 from application.auth.models import User
 from application.user.forms import UserForm
+
 @app.route("/user/new")
 def signup_form():
     return render_template("user/new.html", form = UserForm())
@@ -21,5 +22,12 @@ def signup():
 
     return redirect(url_for("films_index"))
 
-##@app.route("/user/<user_id>", methods=["GET"])
-##def user_profile(user_id):
+@app.route("/user/<user_id>", methods=["GET"])
+def user_profile(user_id):
+    name = User.query.get(user_id).username
+    favorite_films = User.favorite_films(user_id)
+    favorite_actors = User.favorite_actors(user_id)
+    favorite_directors = User.favorite_directors(user_id)
+
+    reviews = User.user_reviews(user_id)
+    return render_template("user/profile.html", name = name, favorite_films = favorite_films, favorite_actors = favorite_actors, favorite_directors = favorite_directors, reviews = reviews)
