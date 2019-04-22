bc410aad3fe2d752ec827fc30106b971e3a796d0
antlammi <antti.lammi@helsinki.fi>
2019-04-22 23:18:15 +0300

    users can edit their own information, navbar visuals enhanced

--- application/user/forms.py
+++ application/user/forms.py
@@ -8,3 +8,16 @@ class UserForm(FlaskForm):
     bio = TextAreaField("Bio", [validators.optional(), validators.length(max=1200)])
     class Meta:
         csrf = False
+
+class EditUserForm(FlaskForm):
+    name = StringField("Name", [validators.length(min=3, max=144)])
+    username = StringField("Username", [validators.length(min=3, max=144)])
+    bio = TextAreaField("Bio", [validators.optional(), validators.length(max=1200)])
+    class Meta:
+        csrf = False
+
+class ChangePassword(FlaskForm):
+    password = PasswordField('New Password', [validators.InputRequired(), validators.EqualTo('confirm', message='Passwords must match'), validators.length(min=3, max=144)])
+    confirm  = PasswordField('Repeat Password')
+    class Meta:
+        csrf = False
--- application/user/views.py
+++ application/user/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
-from application import app, db
+from application import app, db, login_manager, login_required
 from application.auth.models import User
-from application.user.forms import UserForm
+from application.user.forms import UserForm, EditUserForm, ChangePassword
 
 @app.route("/user/new")
 def signup_form():
@@ -21,6 +21,63 @@ def signup():
     db.session().commit()
 
     return redirect(url_for("welcome"))
+
+@app.route("/user/<user_id>/change_password", methods=["GET"])
+@login_required(role="DEFAULT")
+def change_password(user_id):
+    user = User.query.get(user_id)
+    if user.id != current_user.id:
+        return login_manager.unauthorized()
+    return render_template("user/passwordform.html", form = ChangePassword(), user_id = user_id)
+
+
+@app.route("/user/<user_id>/change_password", methods=["POST"])
+@login_required(role="DEFAULT")
+def update_password(user_id):
+    user = User.query.get(user_id)
+    if user.id != current_user.id:
+        return login_manager.unauthorized()
+    form = ChangePassword(request.form)
+   
+    user.password = form.password.data
+    if not form.validate():
+        return render_template("user/passwordform.html", form=form, user_id = user_id)
+    db.session().add(user)
+    db.session().commit()
+
+    return redirect(url_for("welcome"))
+
+@app.route("/user/<user_id>/edit", methods=["GET"])
+@login_required(role="DEFAULT")
+def user_settings(user_id):
+    user = User.query.get(user_id)
+    if (user.id != current_user.id):
+        return login_manager.unauthorized()
+    formtorender = EditUserForm()
+    
+    formtorender.name.data = user.name
+    formtorender.username.data = user.username
+
+    formtorender.bio.data = user.bio
+    return render_template("user/update.html", form=formtorender, user_id = user_id)
+
+@app.route("/user/<user_id>/", methods=["POST"])
+@login_required(role="DEFAULT")
+def update_user(user_id):
+    form = EditUserForm(request.form)
+    u = User.query.get(user_id)
+    if u.id != current_user.id:
+        return login_manager.unauthorized()
+    
+    u.name = form.name.data
+    u.username = form.username.data
+    u.bio = form.bio.data
+    if not form.validate():
+        return render_template("user/update.html", form = form, user_id = user_id)
+    db.session().add(u)
+    db.session().commit()
+    
+    return redirect(url_for("user_profile", user_id = user_id))
 
 @app.route("/user/<user_id>", methods=["GET"])
 def user_profile(user_id):
